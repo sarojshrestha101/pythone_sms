d91e4c7e44d015fa7cf7fbb571e0ccdf588e72d6
Unknown <saroj.shr911@gmail.com>
2017-05-13 13:39:37 +0545

    add send and get sms function

--- __init__.py
+++ __init__.py
@@ -1,8 +1,21 @@
 import time
 import os
 
+def get_allsms():
+    getsms = os.system("sudo gammu getallsms").read()
+    print(getsms)
+
+def send_sms():
+    os.system("clear")
+    sms_text = input("Enter message\n")
+    sms_no = int(input("Enter Phone no\n"))
+
+    os.system("echo {} | gammu sendsms TEXT {}".format(sms_text, sms_no))
+
 def main():
         print("main function")
+        # get_allsms()
+        send_sms()
 
 
 if __name__ == '__main__':
